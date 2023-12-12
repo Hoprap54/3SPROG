3d9f208c29ef903d58fbdd7eef4b7eb427d4c754
Hoprap <benitopmwk@gmail.com>
2023-12-12 12:53:41 +0100

    Corrections for battery voltage

--- Battery_measurement/src/main.c
+++ Battery_measurement/src/main.c
@@ -2,8 +2,11 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "usart.h"
-
-#define ADC_PIN0 0
+/* Voltage didiver
+*   R1: 2.7k
+*   R2: 1k
+*/
+#define ADC_PIN0 6
 
 uint16_t adc_read(uint8_t adc_channel);
 float voltagecalc(void);
@@ -15,7 +18,6 @@ float Volt, totalvolt;
 int main(void){
     uart_init();
     io_redirect();
-    
     ADMUX = (1<<REFS0); // Select Vref = AVcc
     //set prescaler to 128 and turn on the ADC module
     ADCSRA = (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
@@ -29,7 +31,7 @@ int main(void){
 }
 
 uint16_t adc_read(uint8_t adc_channel){
-    ADMUX &= 0xf0;        // Clear any previously used channel
+    ADMUX &= 0x00;        // Clear any previously used channel
     ADMUX |= adc_channel; // set the desired channel
     ADCSRA |= (1<<ADSC);  // Start a conversion
     while ( (ADCSRA & (1<<ADSC)));          // Wait for the conversion to complete
@@ -40,9 +42,8 @@ uint16_t adc_read(uint8_t adc_channel){
 }
 
 float voltagecalc(void){
-   digitalVolt = adc_read(ADC_PIN0);
-   Volt = (float)digitalVolt/1023.0*(4.96-1.0);
-   totalvolt = Volt/15.0*45.0;
-
+    digitalVolt = adc_read(ADC_PIN0);
+    Volt = (float)digitalVolt/1024*11.98;
+    totalvolt = Volt/3.7;
     return totalvolt;
 }
