1a549be89eba76bcfa45146f178306ecd9c7065a
Hoprap <benitopmwk@gmail.com>
2023-12-14 12:57:09 +0100

    Update

--- MCP9071A/src/main.c
+++ MCP9071A/src/main.c
@@ -13,6 +13,7 @@ const float v_0 = 400;   // Output voltage at 0ºC is 400mV *Given in datasheet
 const float tc = 19.5;   // mv/ºC *Given in datasheet
 float ta = 20;  // Ambient temperature 20ºC
 float vout;
+int value;
 
 int main(void){
     uart_init();
@@ -20,7 +21,7 @@ int main(void){
 
     DDRC &= (1<<DDC0);
 
-    ADMUX = (1<<REFS0); // Select Vref = AVcc
+    ADMUX = (1<<REFS0) | (1<<ADLAR); // Select Vref = AVcc and Shift the result to the left
     // Set prescaler to 128 and turn on the ADC module
     ADCSRA = (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
     
@@ -31,19 +32,20 @@ int main(void){
 }
 
 uint16_t adc_read(uint8_t adc_channel){
-    ADMUX |= 0Xf0;
     ADMUX |= adc_channel;
-    ADCSRA |= (1<<ADSC);  // Start a conversion
+    ADCSRA |= (1<<ADSC);         // Start a conversion
     while((ADCSRA & (1<<ADSC))); // Wait for the conversion to complete
-    //ADCSRA|=(1<<ADIF);   // clear ADIF when conversion complete by writing 1
-	return ADC;			/* Return digital value*/
+    _delay_us(10);
+    value = ADCH;
+    printf("ADCH:%d ", value);
+	return(value); 
 }
 
 float tempCalc(void){
     vout = adc_read(ADC_PIN0) * (5000.0/1024);
     ta = (vout - v_0) / tc;
 
-    printf("Temp:%fºC   V:%fmV\n", ta, vout);
+    printf("Temp:%.2f ºC   V:%.2fmV\n", ta, vout);
     _delay_ms(1000);
     return 0;
 }
