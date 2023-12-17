f1d27f51e7d250d965195318b47e635eae1187fb
Hoprap <benitopmwk@gmail.com>
2023-12-17 18:13:03 +0100

    Two timers added

--- Two motors sync/src/main.c
+++ Two motors sync/src/main.c
@@ -103,20 +103,35 @@ int main() {
     return 0;
 }*/
 
-void PWM_Motor(unsigned char);
+void PWM_MotorR(unsigned char);
+void PWM_MotorL(unsigned char duty);
 
 int main(void){
     uart_init();
     while (1){
-        PWM_Motor(100);     // Select speed from 1-255. + number + speed
+        PWM_MotorR(100);     // Select speed from 1-255. + number + speed
+        PWM_MotorL(100);     // Select speed from 1-255. + number + speed
     }
 }
 
-inline void PWM_Motor(unsigned char duty){ 
+/*TIMER0*/
+inline void PWM_MotorR(unsigned char duty){ 
     DDRD |= (1 << PB2);     // Declare this ENA pin as output     
     TCCR0A |= (1<<WGM01) | (1<<WGM00);  // Fast PWM configuration
     TCCR0A |= (1<<COM0A1);              // Non inverting output
 
     TCCR0B |= (1<<CS02) | (1<<CS00);    // 1024 Prescaler
     OCR0A = duty; // Set the value to be compared
+}
+
+/*TIMER1*/
+void PWM_MotorL(unsigned char duty){
+    DDRB |= (1 << PB3);     // Declare this ENA pin as output     
+
+    TCCR1A |= (1<<WGM11) | (1<<WGM10);  // Fast PWM configuration
+    TCCR1B |= (1<<WGM13) | (1<<WGM12);
+    TCCR1A |= (1<<COM1A1);              // Non inverting output
+
+    TCCR1B |= (1<<CS12) | (1<<CS10);    // 1024 Prescaler
+    OCR1A = duty; // Set the value to be compared
 }
